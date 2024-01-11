1dc726947df818693feeba15d95e038a7ff2c7bc
jd.martinezblasco <jd.martinezblasco@10.2.1.19>
2024-01-11 16:25:32 +0100

    Cambios clase EjerciciosClase

---
+++ src/main/java/com/mycompany/ejerciciosclase/EjerciciosClaseDam.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejerciciosclase;
+
+/**
+ *
+ * @author jd.martinezblasco
+ */
+public class EjerciciosClaseDam {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+ 
+    } 
+}
