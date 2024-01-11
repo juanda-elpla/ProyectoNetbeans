04b4d500c4df959c64f9206096485072bea0705a
EDU\denbar3 <denbar3@alu.edu.gva.es>
2024-01-11 18:13:22 +0100

    commit2

--- src/main/java/com/mycompany/ejerciciosclase/EjerciciosClaseDam.java
+++ src/main/java/com/mycompany/ejerciciosclase/EjerciciosClaseDam.java
@@ -20,6 +20,7 @@ public class EjerciciosClaseDam {
          System.out.println("Hello World!");
          System.out.println("Hello World!");
          System.out.println("Hello World!");
-         System.out.println("Hello World2222!");
+         System.out.println("Hello World2222!!!!");
+         
     } 
 }
