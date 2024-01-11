55558327649cc4430aecb60eef356e88d2ff9ee0
jd.martinezblasco <jd.martinezblasco@10.2.1.19>
2024-01-11 18:39:16 +0100

    Problemas solucionado. Madeley odia a Angel

--- src/main/java/com/mycompany/ejerciciosclase/EjerciciosClaseDam.java
+++ src/main/java/com/mycompany/ejerciciosclase/EjerciciosClaseDam.java
@@ -20,17 +20,12 @@ public class EjerciciosClaseDam {
          System.out.println("Hello World!");
          System.out.println("Hello World!");
          System.out.println("Hello World!");
-<<<<<<< HEAD
          System.out.println("Hello World2222!!!!");
           System.out.println("Hello World23");
            System.out.println("Hello World234!!!!");
          
-=======
          System.out.println("Hello World2222!");
          System.out.println("Hello soyÀngelXD");
-         System.out.println("VaDeLokosXD");
->>>>>>> df5a34d30326d44c764fc40df595f0cc270d591b
-        
-        hola;
+         System.out.println("VaDeLokosXD");     
     } 
 }
