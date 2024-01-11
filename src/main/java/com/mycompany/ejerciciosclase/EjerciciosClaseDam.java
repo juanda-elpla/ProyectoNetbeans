e9bbdaea187c3af863d4e8e82cfc1d51b47e4183
EDU\denbar3 <denbar3@alu.edu.gva.es>
2024-01-11 18:34:18 +0100

    Merge branch 'master' of https://github.com/juanda-elpla/proyectonetbeans
    # Conflicts:
    #	src/main/java/com/mycompany/ejerciciosclase/EjerciciosClaseDam.java.

--- src/main/java/com/mycompany/ejerciciosclase/EjerciciosClaseDam.java
+++ src/main/java/com/mycompany/ejerciciosclase/EjerciciosClaseDam.java
@@ -30,5 +30,7 @@ public class EjerciciosClaseDam {
          System.out.println("Hello soyÀngelXD");
          System.out.println("VaDeLokosXD");
 >>>>>>> df5a34d30326d44c764fc40df595f0cc270d591b
+        
+        hola;
     } 
 }
