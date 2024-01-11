4cdaf1dc6520a9912fb23721a040ec74c2350cd5
EDU\denbar3 <denbar3@alu.edu.gva.es>
2024-01-11 18:50:03 +0100

    CAmbio

--- src/main/java/com/mycompany/ejerciciosclase/EjerciciosClaseDam.java
+++ src/main/java/com/mycompany/ejerciciosclase/EjerciciosClaseDam.java
@@ -20,8 +20,28 @@ public class EjerciciosClaseDam {
          System.out.println("Hello World!");
          System.out.println("Hello World!");
          System.out.println("Hello World!");
+<<<<<<< HEAD
+
+=======
          System.out.println("Hello World2222!!!!");
+>>>>>>> ba02cef21050a06cef4b35cd3a4913a6f30e11ab
           System.out.println("Hello World23");
+           System.out.println("Hello World234!!!!");
          
+         System.out.println("Hello World2222!");
+         System.out.println("Hello World!1105");
+         System.out.println("Holiii World!");
+         System.out.println("Hello soyÀngelXD");
+<<<<<<< HEAD
+         System.out.println("VaDeLokosXD");
+<<<<<<< HEAD
+        
+
+=======
+         System.out.println("Holii");
+=======
+         System.out.println("VaDeLokosXD");     
+>>>>>>> 55558327649cc4430aecb60eef356e88d2ff9ee0
+>>>>>>> ba02cef21050a06cef4b35cd3a4913a6f30e11ab
     } 
 }
